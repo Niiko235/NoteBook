6f9a073ea162d975aaf476aa6b553c45e9eaeab3
niiko235 <nicolaseplaza@gmail.com>
2025-03-02 17:12:59 -0500

    segundo commit

---
+++ Tools/template.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+
+#define vi vector<int>
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(x) int(x.size())
+#define all(x) x.begin(),x.end()
+#define forn(i,n) for(int i = 0; i < n; i++)
+#define endl '\n'
+
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+               
+                
+    return(0);
+}
